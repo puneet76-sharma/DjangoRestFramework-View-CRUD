8d23a1efd1dc06ef353a2d6bc0a04105553aebdd
Puneet Sharma <69373560+puneet76-sharma@users.noreply.github.com>
2022-05-14 03:50:17 +0530

    Create install_dependencies.sh

---
+++ install_dependencies.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /home/ubuntu
+sudo apt update
+sudo apt install python3-pip python3-dev nginx -y
+pip install virtualenv
+virtualenv venv
+source venv/bin/activate
+pip install django gunicorn
