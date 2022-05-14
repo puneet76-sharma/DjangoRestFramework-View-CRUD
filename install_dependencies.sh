d30bbf5712b9a02858207c1e1f9a989a360685eb
Puneet Sharma <69373560+puneet76-sharma@users.noreply.github.com>
2022-05-14 16:02:02 +0530

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 rm -rf /home/ubuntu/*
-cd /home/ubuntu/
+cd /home/ubuntu
 sudo apt update
 sudo apt install python3-pip python3-dev nginx -y
 pip install virtualenv
-virtualenv venv
-source venv/bin/activate
+virtualenv myprojectenv
+source myprojectenv/bin/activate
 pip install django gunicorn
