a401b2643d869a9e88458d5d9de947f132f47b22
Puneet Sharma <69373560+puneet76-sharma@users.noreply.github.com>
2022-05-14 17:21:08 +0530

    Update install_project_dependencies.sh

--- install_project_dependencies.sh
+++ install_project_dependencies.sh
@@ -2,19 +2,50 @@
 
 deactivate
 
-mv /home/ubuntu/gunicorn.socket /etc/systemd/system/
+echo "[Unit]
+Description=gunicorn socket
+[Socket]
+ListenStream=/run/gunicorn.sock
+[Install]
+WantedBy=sockets.target
+" > /etc/systemd/system/gunicorn.socket
 
-mv /home/ubuntu/gunicorn.service /etc/systemd/system/
+echo " [Unit]
+Description=gunicorn daemon
+Requires=gunicorn.socket
+After=network.target
+[Service]
+User=ubuntu
+Group=www-data
+WorkingDirectory=/home/ubuntu/project
+ExecStart=/home/ubuntu/venv/bin/gunicorn \
+          --access-logfile - \
+          --workers 3 \
+          --bind unix:/run/gunicorn.sock \
+          ViewDemo_Project.wsgi:application
+[Install]
+WantedBy=multi-user.target"  >  /etc/systemd/system/gunicorn.service
 
 sudo systemctl start gunicorn.socket
 
 sudo systemctl enable gunicorn.socket
 
 
-mv /home/ubuntu/configruation /etc/nginx/sites-available/
+echo "server {
+    listen 80;
+    server_name puneet.co.vu;
+    location = /favicon.ico { access_log off; log_not_found off; }
+    location /static/ {
+        root /home/ubuntu/project;
+    }
+    location / {
+        include proxy_params;
+        proxy_pass http://unix:/run/gunicorn.sock;
+    }
+}" > /etc/nginx/sites-available/ViewDemo_Project
 
 
-sudo ln -s /etc/nginx/sites-available/configruation /etc/nginx/sites-enabled/
+sudo ln -s /etc/nginx/sites-available/ViewDemo_Project  /etc/nginx/sites-enabled/
 
 sudo rm /etc/nginx/sites-enabled/default
 
