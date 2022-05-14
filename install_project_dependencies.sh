98472f20c38de627acdb5d9f5bbbccd23ddbdb6d
Puneet Sharma <69373560+puneet76-sharma@users.noreply.github.com>
2022-05-14 16:27:47 +0530

    Update install_project_dependencies.sh

--- install_project_dependencies.sh
+++ install_project_dependencies.sh
@@ -1,27 +1,23 @@
 #!/bin/bash 
 
-cd /home/ubuntu
+deactivate
 
-source venv/bin/activate
+mv /home/ubuntu/gunicorn.socket /etc/systemd/system/
 
-cd /home/ubuntu/project
-pip install -r requirements.txt
+mv /home/ubuntu/gunicorn.service /etc/systemd/system/
 
-sudo fuser -k 8000/tcp
+sudo systemctl start gunicorn.socket
 
-gunicorn --bind 0.0.0.0:8000 first.wsgi &>/dev/null & 
+sudo systemctl enable gunicorn.socket
 
-# mv /home/ubuntu/gunicorn.socket /etc/systemd/system/
-# mv /home/ubuntu/gunicorn.service /etc/systemd/system/
 
-# sudo systemctl start gunicorn.socket
-# sudo systemctl enable gunicorn.socket
+mv /home/ubuntu/configruation /etc/nginx/sites-available/
 
-mv /home/ubuntu/NGINX /etc/nginx/sites-available 
 
-sudo ln -s /etc/nginx/sites-available/NGINX /etc/nginx/sites-enabled/
+sudo ln -s /etc/nginx/sites-available/configruation /etc/nginx/sites-enabled/
 
-sudo rm /etc/nginx//sites-enabled/default
+sudo rm /etc/nginx/sites-enabled/default
+
 
 sudo systemctl restart nginx
 sudo systemctl restart gunicorn
